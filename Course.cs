97dfe3acbef6aa4eeb82ebf4dbfa190c810ab921
junrielcasul2k2 <113318034+junrielcasul2k2@users.noreply.github.com>
2024-03-20 22:03:36 +0800

    Add files via upload

---
+++ Course.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Course : Registration   
+    {
+        private string coursedesc;
+        private int yearLevel;
+
+        public Course(string coursedesc, int yearLevel,string Fname,string lName, int age) 
+                        : base(Fname,lName,age)
+        {
+            this.coursedesc = coursedesc;
+            this.yearLevel = yearLevel;
+        }
+
+        public string getCoursedesc() 
+        {
+            return this.coursedesc;
+        }
+        public int getyearLevel() 
+        {
+            return this.yearLevel;
+        }
+        public override void displayInfo()
+        {
+            base.displayInfo();
+            Console.WriteLine();
+            Console.WriteLine("\nCourse: " +coursedesc+ "\nYearLevel: " +yearLevel);
+        }
+
+    }
+}
